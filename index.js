da1292364f77b5104296ad6c924c9d22b9435128
Andres Pena <penchjpc@gmail.com>
2022-01-04 14:51:47 -0500

    fixed links to css and js files

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
 const score = document.getElementById('score')
+
+console.log(score)
+console.log(grid)
