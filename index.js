cf3e369a5f3d41c7c05390425d42a615b37e6dfd
Andres Pena <penchjpc@gmail.com>
2022-01-04 16:29:18 -0500

    function & styling to create grid of blue squares

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
 const score = document.getElementById('score')
-
-console.log(score)
-console.log(grid)
+const squares = []
+// Function to draw the grid
+function createGrid() {
+   
+    for (let i=0; i < 100; i++) {
+    
+    const square = document.createElement('div')
+    console.log(square)
+    
+    square.classList.add('square')
+    
+    grid.appendChild(square)
+       
+    squares.push(square)
+    }
+    console.log(squares)
+}
+createGrid()
